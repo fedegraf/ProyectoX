6dfe0897f174733e4c0301c932ba143cce3938fd
Ariel Irala <arriel_sdk-68@hotmail.com>
2022-05-13 20:21:17 -0300

    rew

---
+++ ProyectoX/Intermediate/Source/ProyectoX.Build.cs
@@ -0,0 +1,12 @@
+using UnrealBuildTool;
+
+public class ProyectoX : ModuleRules
+{
+	public ProyectoX(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PrivateDependencyModuleNames.Add("Core");
+		PrivateDependencyModuleNames.Add("Core");
+	}
+}
